4a0411342ebf302c13dab5a8e4bf6e4ce7e5e326
riccardo-ep <riccardo.larosa@elaticpath.com>
2021-05-13 16:08:25 -0400

    augmented data for cart and cartItems

--- src/resolvers/queries/cart.ts
+++ src/resolvers/queries/cart.ts
@@ -1,8 +1,14 @@
-const cart = async (root, {id: cartId}, {Moltin}) => {
+const cart = async (parent, { id: cartId }, { Moltin }) => {
     const getCart = Moltin.Cart(cartId).Get()
     const getCartItems = Moltin.Cart(cartId).Items()
-    const [{data: {id}}, {data: items}] = await Promise.all([getCart, getCartItems])
-    return {id, items}
+    const [{ data: cartData }, { data: itemsData }] = await Promise.all([getCart, getCartItems])
+    return {
+        ...cartData,
+        priceWithTax: cartData.meta.display_price.with_tax,
+        priceWithoutTax: cartData.meta.display_price.without_tax,
+        tax: cartData.meta.display_price.tax,
+        items: itemsData,
+    }
 }
 
 export default {
