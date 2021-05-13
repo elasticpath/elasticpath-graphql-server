cd1d8e2f80482bd470ddde3c0d891be04c7b9c67
Deepak Azad <deepak.azad@elasticpath.com>
2021-05-13 15:33:37 -0700

    feat: pcm

--- src/resolvers/queries/pcmProducts.ts
+++ src/resolvers/queries/pcmProducts.ts
@@ -1,16 +1,27 @@
-const pcmProducts = async (parent, args, {Moltin}) => {
+const pcmProducts = async (parent, { token }, {Moltin}) => {
     try {
-        const {data: products} = await Moltin.PCM.All()
-        return products
+        const ELASTICPATH_API_HOST = process.env.ELASTICPATH_API_HOST
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+        
+        const data = await fetch(`https://`+ELASTICPATH_API_HOST+`/catalog/products`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
     } catch (e) {
         return e
     }
 }
 
-const pcmProduct = async (parent, {id}, {Moltin}) => {
+const pcmProduct = async (parent, {id , token}, {Moltin}) => {
     try {
-        const {data: product} = await Moltin.PCM.Get(id)
-        return product
+        const {data: product} = await Moltin.Catalogs.Products.Get(id, token)
+        return {...product }
     } catch (e) {
         return e
     }
