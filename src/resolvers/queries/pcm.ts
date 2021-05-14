e0698927df939136f13a8b6053459ac1289fd1ee
dazadep <62723515+dazadep@users.noreply.github.com>
2021-05-13 21:15:12 -0700

    feat: basic pcm support - products, nodes, herarchies (#36)

---
+++ src/resolvers/queries/pcm.ts
@@ -0,0 +1,124 @@
+const host = process.env.ELASTICPATH_API_HOST
+
+const pcmProducts = async (parent, args, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/products`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+const pcmProduct = async (parent, {id}, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/products/` + id, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+const nodes = async (parent, args, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/nodes`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+const node = async (parent, {id}, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/nodes/` + id, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+const hierarchies = async (parent, args, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/hierarchies`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+const hierarchy = async (parent, {id}, {Moltin}) => {
+    try {
+        const authHeader = "Bearer " + JSON.parse(Moltin.storage.get("moltinCredentials")).access_token
+
+        const data = await fetch(`https://` + host + `/catalog/hierarchies/` + id, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                "Authorization": authHeader
+            },
+        })
+        const result = await data.json()
+        return result.data
+    } catch (e) {
+        return e
+    }
+}
+
+export default {
+    pcmProducts,
+    pcmProduct,
+    nodes,
+    node,
+    hierarchies,
+    hierarchy
+}
