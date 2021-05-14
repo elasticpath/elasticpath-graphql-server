58ddd06e6715a2a34d392766969fa595429a4143
Deepak Azad <deepak.azad@elasticpath.com>
2021-05-13 19:55:44 -0700

    renames

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -6,7 +6,7 @@ import customerMutationResolvers from './mutations/customers'
 import cartQueryResolvers from './queries/cart'
 import brandQueryResolvers from './queries/brands'
 import productQueryResolvers from './queries/products'
-import pcmProductQueryResolvers from './queries/pcmProducts'
+import pcmQueryResolvers from './queries/pcm'
 import categoryQueryResolvers from './queries/categories'
 import collectionQueryResolvers from './queries/collections'
 import customerQueryResolvers from './queries/customers'
@@ -24,7 +24,7 @@ export default {
     ...brandQueryResolvers,
     ...cartQueryResolvers,
     ...productQueryResolvers,
-    ...pcmProductQueryResolvers,
+    ...pcmQueryResolvers,
     ...categoryQueryResolvers,
     ...collectionQueryResolvers,
     ...customerQueryResolvers
