7f6a3de166cd2c9f8a58427ba95671bc7f0d094e
Deepak Azad <deepak.azad@elasticpath.com>
2021-05-15 07:19:13 -0700

    feat: get implicit token

---
+++ src/datasources/TokensDataSource.ts
@@ -0,0 +1,32 @@
+import {RESTDataSource} from "apollo-datasource-rest";
+
+export class TokensDataSource extends RESTDataSource {
+    willSendRequest(request) {
+        request.headers.set('Accept', 'application/json')
+    }
+
+    constructor() {
+        super()
+        this.baseURL = `https://${process.env.ELASTICPATH_API_HOST}`
+    }
+
+    async authenticate(client_id) {
+        const body = {
+            client_id: client_id,
+            grant_type: 'implicit'
+        }
+        const result = await this.post(
+            `/oauth/access_token`,
+            Object.keys(body)
+                .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(body[k])}`)
+                .join('&'),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            }
+        )
+        return result
+    }
+
+}
