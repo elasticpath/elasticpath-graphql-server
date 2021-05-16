6ed7669a4780c9a29b1b4c27815173726c8007bc
Deepak Azad <deepak.azad@elasticpath.com>
2021-05-15 21:07:28 -0700

    feat: pass customer token as a header instead of a parameter

--- src/resolvers/queries/customers.ts
+++ src/resolvers/queries/customers.ts
@@ -1,25 +1,25 @@
 import {UserInputError} from "apollo-server";
 
-const customer = async (parent, { id, token }, { Moltin }) => {
+const customer = async (parent, { id, token }, { Moltin, req }) => {
     try {
-        const { data: customer } = await Moltin.Customers.Get(id, token)
+        const { data: customer } = await Moltin.Customers.Get(id, req.headers['x-moltin-customer-token'])
         return customer
     } catch (e) {
         throw new UserInputError("API returned with errors.", e)
     }
 }
-const customerAddresses = async (parent, { customer, token }, { Moltin }) => {
+const customerAddresses = async (parent, { customer, token }, { Moltin, req }) => {
     try {
-        const { data: addresses } = await Moltin.Addresses.All({ customer, token }, )
+        const { data: addresses } = await Moltin.Addresses.All({ customer: customer, token: req.headers['x-moltin-customer-token'] } )
         return addresses
     } catch (e) {
         throw new UserInputError("API returned with errors.", e)
     }
 }
 
-const customerAddress = async (parent, { customer, address: addressInput, token}, { Moltin }) => {
+const customerAddress = async (parent, { customer, address: addressInput, token}, { Moltin, req }) => {
     try {
-        const { data: address } = await Moltin.Addresses.Get({ customer, address: addressInput, token })
+        const { data: address } = await Moltin.Addresses.Get({ customer: customer, address: addressInput, token: req.headers['x-moltin-customer-token'] })
         return address
     } catch (e) {
         throw new UserInputError("API returned with errors.", e)
