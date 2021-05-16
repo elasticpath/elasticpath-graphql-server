fdc8a1221fa9e195d035b674cd16d836a6ad5441
dazadep <62723515+dazadep@users.noreply.github.com>
2021-05-16 10:14:49 -0700

    feat: orders (#58)
    * refactor: separate orders from carts
    
    * feat: orders

---
+++ src/datasources/OrdersDataSource.ts
@@ -0,0 +1,26 @@
+import {RESTDataSource} from "apollo-datasource-rest";
+
+export class OrdersDataSource extends RESTDataSource {
+  willSendRequest(request) {
+    request.headers.set('Authorization', this.context.req.headers.authorization)
+    request.headers.set('X-Moltin-Customer-Token', this.context.req.headers['x-moltin-customer-token'])
+    request.headers.set('Content-Type', 'application/json')
+    request.headers.set('Accept', 'application/json')
+  }
+
+  constructor() {
+    super();
+    this.baseURL = `https://${process.env.ELASTICPATH_API_HOST}/v2`
+  }
+
+  async getOrders(id) {
+    const { data: result } = await this.get(`/orders/`);
+    return result;
+  }
+  
+  async getOrder(id) {
+    const { data: result } = await this.get(`/orders/${id}/`);
+    return result;
+  }
+
+}
