d9441acf20c5c254b43c4dd84fa69a8360f51d07
Parvez Akkas <parvez.akkas@elasticpath.com>
2021-05-18 09:49:21 -0400

    chore (test): add html and junit reporter for test results

--- newman-config.js
+++ newman-config.js
@@ -2,12 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   // Set path to collection and corresponding environment variables
-  collection: "postman/EP-GraphQL-Test.postman_collection.json",
-  environment: "postman/EP-GraphQL-env.postman_environment.json",
-  globals: "postman/EP-GraphQL-globals.postman_globals.json",
+  collection: './postman/EP-GraphQL-Test.postman_collection.json',
+  environment: './postman/EP-GraphQL-env.postman_environment.json',
+  globals: './postman/EP-GraphQL-globals.postman_globals.json',
 
-  // Reporting configurations
-  reporters: 'cli',
+  // Set reporters to be used
+  reporters: [ 'cli', 'htmlextra', 'junit' ],
+
+  // Set reporte configurations
+  reporter: {
+    htmlextra: {
+      export: './build/reports/postman-collection-report.html',
+      browserTitle: 'EPCC Graphql API Test Report',
+      title: 'EPCC Graphql API Test Report',
+    },
+    junit: {
+      export: './build/reports/postman-collection-report-junit.xml'
+    }
+  },
 
   // Overwrite or set some of the variables’s initial values
   globalVar: [
