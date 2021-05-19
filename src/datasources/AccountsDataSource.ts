2a65563c5e2dd0ddf6ed4321b6ccd6af19d07d1d
danial-ep <danial.javanmardi@elasticpath.com>
2021-05-19 12:51:05 -0700

    Revert back a file

--- src/datasources/AccountsDataSource.ts
+++ src/datasources/AccountsDataSource.ts
@@ -1,31 +1,30 @@
-import {RESTDataSource} from "apollo-datasource-rest"
+import {RESTDataSource} from "apollo-datasource-rest";
 
 export class AccountsDataSource extends RESTDataSource {
-    willSendRequest(request) {
-        request.headers.set('Authorization', this.context.req.headers.authorization)
-        request.headers.set('Content-Type', 'application/json')
-        request.headers.set('Accept', 'application/json')
-        if (this.context.req.headers['ep-beta-features']) {
-            request.headers.set('EP-Beta-Features', this.context.req.headers['ep-beta-features'])
-        }
-        if (this.context.req.headers['ep-account-management-authentication-token']) {
-            request.headers.set('EP-Account-Management-Authentication-Token', this.context.req.headers['ep-account-management-authentication-token'])
-        }
+  willSendRequest(request) {
+    request.headers.set('Authorization', this.context.req.headers.authorization)
+    request.headers.set('Content-Type', 'application/json')
+    request.headers.set('Accept', 'application/json')
+    if(this.context.req.headers['ep-beta-features']){
+      request.headers.set('EP-Beta-Features', this.context.req.headers['ep-beta-features'])
     }
+    if(this.context.req.headers['ep-account-management-authentication-token']){
+      request.headers.set('EP-Account-Management-Authentication-Token', this.context.req.headers['ep-account-management-authentication-token'])
+    }
+  }
 
-    constructor() {
-        super();
-        this.baseURL = `https://${process.env.ELASTICPATH_API_HOST}/v2`;
-    }
+  constructor() {
+    super();
+    this.baseURL = `https://${process.env.ELASTICPATH_API_HOST}/v2`
+  }
 
-    async generateAccountTokens(accountTokenInput) {
-        accountTokenInput.type = 'account_management_authentication_token'
-        const {data} = await this.post(`/account-members/tokens`, {data: accountTokenInput});
-        return data;
-    }
-
-    async accountAuthenticationSetting() {
-        const {data} = await this.get(`/settings/account-authentication`);
-        return data;
-    }
+  async generateAccountTokens(accountTokenInput) {
+    accountTokenInput.type = 'account_management_authentication_token'
+    const {data} = await this.post(`/account-members/tokens`, {data: accountTokenInput});
+    return data;
+  }
+  async accountAuthenticationSetting() {
+    const {data} = await this.get(`/settings/account-authentication`);
+    return data;
+  }
 }
